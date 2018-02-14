5bc71bee59c35e4bab3aa9d17bb1f16ea7167f83
Awiruth Wicharew <awiruththong@hotmail.com>
2018-02-14 13:35:48 +0700

    Test file update.

--- tests.ts
+++ tests.ts
@@ -1,80 +1,43 @@
-{
-  // Drive 100 ms forward
-  bitbot.motor(BBMotor.All, 1023);
-  basic.pause(100);
+input.onButtonPressed(Button.A, () => {
+    IBIT.Turn(turn.Left, 50)
+    basic.pause(1000)
+    IBIT.Turn(turn.Right, 50)
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, () => {
+    IBIT.Spin(spin.Left, 50)
+    basic.pause(1000)
+    IBIT.Spin(spin.Right, 50)
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.AB, () => {
+    basic.showNumber(IBIT.ReadADC(readADC.ADC0))
+    basic.showNumber(IBIT.ReadADC(readADC.ADC1))
+    basic.showNumber(IBIT.ReadADC(readADC.ADC2))
+    basic.showNumber(IBIT.ReadADC(readADC.ADC3))
+    basic.showNumber(IBIT.ReadADC(readADC.ADC4))
+    basic.showNumber(IBIT.ReadADC(readADC.ADC5))
+    basic.showNumber(IBIT.ReadADC(readADC.ADC6))
+    basic.showNumber(IBIT.ReadADC(readADC.ADC7))
+})
 
-  // Drive 100 ms reverse
-  bitbot.motor(BBMotor.All, -1023);
-  basic.pause(100);
-
-  // Drive 100 ms forward on left and reverse on right
-  bitbot.motor(BBMotor.Left, 1023);
-  bitbot.motor(BBMotor.Right, -1023);
-  basic.pause(100);
-
-
-  bitbot.thong(BBMotor.All, 1023);
-  basic.pause(100);
-
-  // Drive 100 ms reverse
-  bitbot.thong(BBMotor.All, -1023);
-  basic.pause(100);
-
-  // Drive 100 ms forward on left and reverse on right
-  bitbot.thong(BBMotor.Left, 1023);
-  bitbot.thong(BBMotor.Right, -1023);
-  basic.pause(100);
-
-
-  // Buzz for 100 ms
-  bitbot.buzz(1);
-  basic.pause(100);
-  bitbot.buzz(0);
-
-  // Read left and right line sensor
-  basic.showNumber(bitbot.readLine(BBLineSensor.Left));
-  basic.showNumber(bitbot.readLine(BBLineSensor.Right));
-
-  // Read left and right light sensor
-  basic.showNumber(bitbot.readLight(BBLightSensor.Left));
-  basic.showNumber(bitbot.readLight(BBLightSensor.Right));
-
-  // Read sonar values
-  basic.showNumber(bitbot.sonar(BBPingUnit.MicroSeconds));
-  basic.showNumber(bitbot.sonar(BBPingUnit.Centimeters));
-  basic.showNumber(bitbot.sonar(BBPingUnit.Inches));
-
-  // Show all leds
-  bitbot.neoSetColor(neopixel.colors(NeoPixelColors.Red));
-  bitbot.neoShow();
-
-  // Clear all leds
-  bitbot.neoClear();
-  bitbot.neoShow();
-
-  // Show led at position 1
-  bitbot.neoSetPixelColor(0, neopixel.colors(NeoPixelColors.Red));
-  bitbot.neoShow();
-
-  // Show led rainbow
-  bitbot.neoRainbow();
-  bitbot.neoShow();
-
-  // Show led rainbow and shift
-  bitbot.neoRainbow();
-  bitbot.neoShift();
-  bitbot.neoShow();
-
-  // Show led rainbow and rotate
-  bitbot.neoRainbow();
-  bitbot.neoRotate();
-  bitbot.neoShow();
-
-  // Set brightness of leds
-  bitbot.neoBrightness(100);
-  bitbot.neoShow();
-
-  // Use neo() variable
-  bitbot.neo().clear();
-  bitbot.neo().show();
-}
+basic.forever(() => {
+    for (let Speed = 0; Speed <= 100; Speed++) {
+        IBIT.Motor(motor.Forward, Speed)
+        basic.pause(50)
+    }
+    basic.pause(1000)
+    for (let Speed = 0; Speed <= 100; Speed++) {
+        IBIT.Motor(motor.Backward, Speed)
+        basic.pause(50)
+    }
+    basic.pause(1000)
+    IBIT.MotorStop()
+    for (let Degree = 0; Degree <= 180; Degree++) {
+        IBIT.Servo(servo.SV1, Degree)
+        IBIT.Servo(servo.SV2, Degree)
+        basic.pause(30)
+    }
+    IBIT.Servo(servo.SV1, 0)
+    IBIT.Servo(servo.SV2, 0)
+})
