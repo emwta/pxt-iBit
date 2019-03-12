c0282bdeea0386f0a894b3205b441038c6d7dcfb
Wicharew <aw250271@ncr.com>
2019-03-12 11:42:30 +0700

    UpdateMotor2

--- iBIT.ts
+++ iBIT.ts
@@ -108,6 +108,7 @@ namespace iBIT {
     }
      /**Motor Block to drives motor forward and backward. The speed motor is adjustable between 0 to 100 and can adjustable speed 2 motors.
       * @param speed1 percent of maximum speed, eg: 50
+      * @param speed2 percent of maximum speed, eg: 50
       */
     //% blockId="ibit_Motor2" block="Motor2 %ibitMotor|speed1 %speed|speed2 %speed"
     //% speed.min=0 speed.max=100
